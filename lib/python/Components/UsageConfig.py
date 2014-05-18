faebc14353c96b310db967c98689be14b255e37b
toto <toto_golden@gmx.de>
2014-05-18 10:05:04 +0200

    Update UsageConfig.py
    all "no timeout" same for translation

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -49,7 +49,7 @@ def InitUsageConfig():
 	choicelist = []
 	for i in range(1, 11):
 		choicelist.append(("%d" % i, ngettext("%d second", "%d seconds", i) % i))
-	config.usage.infobar_timeout = ConfigSelection(default = "5", choices = [("0", _("No timeout"))] + choicelist)
+	config.usage.infobar_timeout = ConfigSelection(default = "5", choices = [("0", _("no timeout"))] + choicelist)
 	config.usage.show_infobar_on_zap = ConfigYesNo(default = True)
 	config.usage.show_infobar_on_skip = ConfigYesNo(default = True)
 	config.usage.show_infobar_on_event_change = ConfigYesNo(default = False)
